ac3ccddc6dd2b25aa495909b2b92d14cb59327bb
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-06-29 19:35:17 -0400

    Move the table tag to parent

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,14 +1,12 @@
 function Word() {
   return (
-    <table className="table border fw-bold text-white table-width">
-      <tr className="row-height">
-        <td className="border text-center bg-success column-width">A</td>
-        <td className="border text-center bg-secondary column-width">P</td>
-        <td className="border text-center bg-warning column-width">P</td>
-        <td className="border text-center bg-secondary column-width">L</td>
-        <td className="border text-center bg-secondary column-width">E</td>
-      </tr>
-    </table>
+    <tr className="row-height">
+      <td className="border text-center bg-success column-width">A</td>
+      <td className="border text-center bg-secondary column-width">P</td>
+      <td className="border text-center bg-warning column-width">P</td>
+      <td className="border text-center bg-secondary column-width">L</td>
+      <td className="border text-center bg-secondary column-width">E</td>
+    </tr>
   );
 }
 
--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -3,7 +3,9 @@ import Word from "./Word";
 function Wordle() {
   return (
     <div className="d-flex justify-content-center">
-      <Word />
+      <table className="table border fw-bold text-white table-width">
+        <Word />
+      </table>
     </div>
   );
 }
