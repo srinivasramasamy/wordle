af08c455df78b4d0b5eea195551c8f0ed7399731
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-06-29 00:16:16 -0400

    Move height style to table level

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,36 +1,24 @@
 function Word() {
   return (
     <>
-      <table className="table border fw-bold text-white" style={{ width: 250 }}>
+      <table
+        className="table border fw-bold text-white"
+        style={{ width: 250, height: 50 }}
+      >
         <tr>
-          <td
-            className="border text-center bg-success"
-            style={{ width: 50, height: 50 }}
-          >
+          <td className="border text-center bg-success" style={{ width: 50 }}>
             A
           </td>
-          <td
-            className="border text-center bg-secondary"
-            style={{ width: 50, height: 50 }}
-          >
+          <td className="border text-center bg-secondary" style={{ width: 50 }}>
             P
           </td>
-          <td
-            className="border text-center bg-warning"
-            style={{ width: 50, height: 50 }}
-          >
+          <td className="border text-center bg-warning" style={{ width: 50 }}>
             P
           </td>
-          <td
-            className="border text-center bg-secondary"
-            style={{ width: 50, height: 50 }}
-          >
+          <td className="border text-center bg-secondary" style={{ width: 50 }}>
             L
           </td>
-          <td
-            className="border text-center bg-secondary text-white"
-            style={{ width: 50, height: 50 }}
-          >
+          <td className="border text-center bg-secondary" style={{ width: 50 }}>
             E
           </td>
         </tr>
