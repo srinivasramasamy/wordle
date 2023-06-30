6b8bc226b861a452879295708d731b8971c798b5
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-06-29 21:52:56 -0400

    Set initial state for wordle component

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { Letter } from "../types/Letter";
+import { Word } from "../types/Word";
 import Row from "./Row";
 
 function Wordle() {
+  const buildEmptyWord = (): Word => {
+    return new Word([
+      new Letter("", ""),
+      new Letter("", ""),
+      new Letter("", ""),
+      new Letter("", ""),
+      new Letter("", ""),
+    ]);
+  };
+  const [words, setWords] = useState<Word[]>([
+    buildEmptyWord(),
+    buildEmptyWord(),
+    buildEmptyWord(),
+    buildEmptyWord(),
+    buildEmptyWord(),
+    buildEmptyWord(),
+  ]);
+
   return (
     <div className="d-flex justify-content-center">
       <table className="table border fw-bold table-width">
-        <Row />
+        {words.map((word) => (
+          <Row word={word} />
+        ))}
       </table>
     </div>
   );
