eaea4efe60f820cf3c09a46f10695faf77554889
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-06-30 15:27:56 -0400

    Add submitted property to word type

--- src/types/Word.ts
+++ src/types/Word.ts
@@ -2,8 +2,10 @@ import { Letter } from "./Letter";
 
 export class Word {
   letters: Letter[];
+  submitted: boolean;
 
-  constructor(letters: Letter[]) {
+  constructor(letters: Letter[], submitted: boolean = false) {
     this.letters = letters;
+    this.submitted = submitted;
   }
 }
