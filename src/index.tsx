52b88ec44f44a38c9d0aca55ab4e984420adf926
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-07-01 19:47:30 -0400

    Send random solution to wordle component

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import Wordle from "./components/Wordle";
+import { solutions } from "./data/Data";
 import "./styles/styles.css";
+
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <h1 className="d-flex justify-content-center">Wordle</h1>
-    <Wordle solution="apple" />
+    <Wordle solution={solutions[getRandomInt(solutions.length)]} />
   </React.StrictMode>,
   document.getElementById("root")
 );
