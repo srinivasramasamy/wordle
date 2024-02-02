8ccdf0bf0765fa58117933538c75c91dfe4c53b8
Srinivas Ramasamy <srinivasramasami@gmail.com>
2024-02-02 15:22:05 -0500

    Add key pad component

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -1,5 +1,13 @@
+import Key from "./Key";
+
 function Keypad() {
-  return <div className="d-flex justify-content-center">Keypad</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="d-flex gap-1">
+        <Key keyChar="Q" keyCode={81} />
+      </div>
+    </div>
+  );
 }
 
 export default Keypad;
